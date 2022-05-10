4e5d53ee6ddd674d4434aa4719cd051ccc4e88e3
Marcos <marcoscti39@gmail.com>
2022-05-10 15:15:06 -0300

    :package: add a point shower

---
+++ src/components/PointMaker/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+function PointMaker(){
+    return /*html */`
+    <section class="points-container">
+        <div class="points"></div>
+        <div class="points"></div>
+        <div class="points"></div>
+    </section>`
+}
+
+export default PointMaker
